82d12232c83c670a2e3af490ab378538cd6efd39
Alfred <somethingfly@somethingfly.com>
2022-11-05 17:38:56 -0700

    commit tree 38db0953ba7a6a2a4b19f73eff85a4563802f3a3

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  // return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I like to steal content by Anthony Clark<br />
+      or people who steal from Anthony Clark<br />
+      or I like people who steal from Anthony Clark<br />
+      or I am not a Web Developer from New York named Liza.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  )
 }
 
 export default About;
